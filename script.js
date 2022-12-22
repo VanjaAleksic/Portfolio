9222f50796e300cb872df5b151feb40734bd5311
VanjaAleksic <vanja.aleksic211@gmail.com>
2022-12-22 16:28:05 +0100

    'first'

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("image-item")){
+          const src = e.target.getAttribute("src");
+          document.querySelector(".modal-img").src = src;
+          const myModal = new bootstrap.Modal(document.getElementById('image-modal'));
+          myModal.show();
+    }
+  })
+  
+  var year = new Date().getFullYear();
+  const footer = document.getElementById("footer-time");
+  footer.innerHTML = year + " Vanja Aleksić";
